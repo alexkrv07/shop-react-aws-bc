3655667acc24a492a64d0f2fa964081635cf6d43
Oleksii Korovushkin <alexkrv07@gmail.com>
2022-07-03 14:35:07 +0300

    feat: implement importProductsFile

---
+++ import-service/handler.js
@@ -0,0 +1,109 @@
+const AWS = require('aws-sdk');
+const BUCKET = 'aws-node-task5';
+// 'use strict';
+
+
+
+module.exports = {
+  // thumbnailsList: async function() {
+  //   const s3 = new AWS.S3({ region: 'eu-west-1' });
+  //   let statusCode = 200;
+  //   let body = {};
+  //   let thumbnails = [];
+  //   const params = {
+  //     Bucket: BUCKET,
+  //     Prefix: 'thumbnails/',
+  //     Delimiter: '/'
+  //   };
+
+  //   try {
+  //     const s3Reasponse = await s3.listObjectsV2(params).promise();
+  //     thumbnails = s3Reasponse.Contents;
+  //     body = JSON.stringify(
+  //       thumbnails
+  //         .filter(thumbnail => thumbnail.Size)
+  //         .map(thumbnail => `https://${ BUCKET }.s3.amazonaws.com/${ thumbnail.Key }`)
+  //     );
+  //   } catch (error) {
+  //     console.error('Error appears:');
+  //     console.error(error);
+  //     statusCode = 500;
+  //     body = error;
+  //   }
+
+  //   return {
+  //     statusCode,
+  //     headers: { 'Access-Control-Allow-Origin': '*' },
+  //     body,
+  //   };
+  // },
+
+  // imageUpload: async function(event) {
+  //   const s3 = new AWS.S3({ region: 'eu-west-1' });
+
+  //   for (const record of event.Records) {
+  //     await s3.copyObject({
+  //       Bucket: BUCKET,
+  //       CopySource: BUCKET + '/' + record.s3.object.key,
+  //       Key: record.s3.object.key.replace('images', 'thumbnails')
+  //     }).promise();
+
+  //     await s3.deleteObject({
+  //       Bucket: BUCKET,
+  //       Key: record.s3.object.key
+  //     }).promise();
+
+  //     console.log('Thumbnails for an image' + record.s3.object.key.split('/')[1] + ' is created');
+  //   }
+
+  //   return {
+  //     statusCode: 202,
+  //   }
+  // },
+
+  importProductsFile: async function(event) {
+    const s3 = new AWS.S3({ region: 'eu-west-1' });
+    let statusCode = 200;
+    let body = {};
+    const pathToFile = event.queryStringParameters.name;
+
+    const catalogPath = `uploaded/${pathToFile}`;
+
+
+    const params = {
+      Bucket: BUCKET,
+      Key: catalogPath,
+      Expires: 60,
+      ContentType: 'text/csv',
+    };
+
+    try {
+      const signedURL = await new Promise((reject, resolve) => {
+        return  s3.getSignedUrl('putObject', params, (err, url) => {
+          if (err || !url) {
+            console.log('My Error');
+            reject(err);
+          }
+          console.log(url)
+
+          resolve(url);
+        });
+      });
+
+        console.log('signedURL: ', signedURL);
+        body = JSON.stringify({signedURL});
+
+    } catch (error) {
+      console.error('Error appears:');
+      console.error(error);
+      statusCode = 500;
+      body = error;
+    }
+
+    return {
+      statusCode,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body,
+    };
+  }
+}
